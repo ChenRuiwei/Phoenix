1d23554300177d97bb17bdc5a69d1fb88d71c72d
crw <92102709+ChenRuiwei@users.noreply.github.com>
2024-02-22 22:14:16 +0800

    refactor: change linker script name to linker.ld

--- kernel/build.rs
+++ kernel/build.rs
@@ -4,7 +4,7 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    let link_script = fs::read_to_string(PathBuf::from(manifest_dir).join("link.ld")).unwrap();
+    let link_script = fs::read_to_string(PathBuf::from(manifest_dir).join("linker.ld")).unwrap();
 
     let ram_size = config::mm::RAM_SIZE - config::mm::KERNEL_OFFSET;
 
@@ -13,7 +13,7 @@ fn main() {
         .replace("%VIRT_START%", &config::mm::KERNEL_START.to_string())
         .replace("%RAM_SIZE%", &ram_size.to_string());
 
-    let dest = PathBuf::from(out_dir).join("link.ld");
+    let dest = PathBuf::from(out_dir).join("linker.ld");
     fs::write(&dest, new).unwrap();
     println!("cargo:rustc-link-arg=-T{}", dest.display());
 }
