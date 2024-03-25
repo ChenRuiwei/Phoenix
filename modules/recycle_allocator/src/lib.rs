23a1cd16d140a6b08a01212158f6517d92d6d4f6
ChenRuiwei <1982833213@qq.com>
2024-03-25 20:49:36 +0800

    feat(allocator): add recycle_allocator crate

---
+++ modules/recycle_allocator/src/lib.rs
@@ -0,0 +1,41 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+use alloc::vec::Vec;
+
+/// Used for allocating pid & tid
+pub struct RecycleAllocator {
+    current: usize,
+    recycled: Vec<usize>,
+}
+
+impl RecycleAllocator {
+    /// Create an empty `RecycleAllocator`
+    pub const fn new(init_val: usize) -> Self {
+        RecycleAllocator {
+            current: init_val,
+            // TODO: use heap to replace vec
+            recycled: Vec::new(),
+        }
+    }
+    /// Allocate an id
+    pub fn alloc(&mut self) -> usize {
+        if let Some(id) = self.recycled.pop() {
+            id
+        } else {
+            self.current += 1;
+            self.current - 1
+        }
+    }
+    /// Recycle an id
+    pub fn dealloc(&mut self, id: usize) {
+        assert!(id < self.current);
+        assert!(
+            !self.recycled.iter().any(|iid| *iid == id),
+            "id {} has been deallocated!",
+            id
+        );
+        self.recycled.push(id);
+    }
+}
