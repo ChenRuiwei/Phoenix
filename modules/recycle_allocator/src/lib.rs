c38eadbae7b65335c06c8a39979f225c2f650d2f
ChenRuiwei <1982833213@qq.com>
2024-03-26 11:32:49 +0800

    feat(allocator): replace vec with heap in recycle_allocator to always alloc the min id

--- modules/recycle_allocator/src/lib.rs
+++ modules/recycle_allocator/src/lib.rs
@@ -1,13 +1,17 @@
 #![no_std]
 #![no_main]
+#![feature(const_binary_heap_constructor)]
 
 extern crate alloc;
-use alloc::vec::Vec;
+use alloc::collections::BinaryHeap;
+use core::cmp::Reverse;
 
 /// Used for allocating pid & tid
 pub struct RecycleAllocator {
+    /// Current max id allocated
     current: usize,
-    recycled: Vec<usize>,
+    /// Hold deallocated id, will be recycled first when alloc happen
+    recycled: BinaryHeap<Reverse<usize>>,
 }
 
 impl RecycleAllocator {
@@ -15,13 +19,12 @@ impl RecycleAllocator {
     pub const fn new(init_val: usize) -> Self {
         RecycleAllocator {
             current: init_val,
-            // TODO: use heap to replace vec
-            recycled: Vec::new(),
+            recycled: BinaryHeap::new(),
         }
     }
     /// Allocate an id
     pub fn alloc(&mut self) -> usize {
-        if let Some(id) = self.recycled.pop() {
+        if let Some(Reverse(id)) = self.recycled.pop() {
             id
         } else {
             self.current += 1;
@@ -32,10 +35,10 @@ impl RecycleAllocator {
     pub fn dealloc(&mut self, id: usize) {
         assert!(id < self.current);
         assert!(
-            !self.recycled.iter().any(|iid| *iid == id),
+            !self.recycled.iter().any(|iid| (*iid).0 == id),
             "id {} has been deallocated!",
             id
         );
-        self.recycled.push(id);
+        self.recycled.push(Reverse(id));
     }
 }
