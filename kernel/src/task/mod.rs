b88437a9edb86d819063dd9b0b62fe87d0e43257
Stone749990226 <749990226@qq.com>
2024-04-05 14:53:48 +0800

    feat(queue):add queue.rs

--- kernel/src/task/mod.rs
+++ kernel/src/task/mod.rs
@@ -2,7 +2,10 @@ pub mod aux;
 mod manager;
 mod pid;
 mod schedule;
+mod queue;
+mod sched;
 pub mod task;
+
 
 pub use schedule::{spawn_kernel_task, spawn_user_task};
 pub use task::Task;
---
+++ kernel/src/task/sched.rs
@@ -0,0 +1,179 @@
+use core::{cell::RefCell, str, sync::atomic::AtomicBool};
+
+use alloc::{boxed::Box, sync::Arc};
+use async_task::Runnable;
+use crossbeam_queue::SegQueue;
+use scoped_tls::scoped_thread_local;
+
+use super::queue::{Local, Steal};
+
+const WORKER_TICK_INTERVAL: u32 = 17;
+
+pub struct Worker {
+    run_queue: Local<Runnable>,
+    preempt_slot: Option<Runnable>,
+}
+
+pub struct  Context {
+    worker: RefCell<Worker>,
+    executor: Arc<Executor>,
+}
+
+/// 所有的工作线程（或处理器）共享同一个Executor实例
+pub struct Executor {
+    injector: SegQueue<Runnable>,
+    stealers: Box<[Steal<Runnable>]>,
+    is_shutdown: AtomicBool,
+}
+
+scoped_thread_local!(pub(crate) static CX: Context);
+
+impl Executor {
+    /// Create a new executor with `num` runners and a `init` future.
+    ///
+    /// The caller should iterate over the returned startup functions and run
+    /// them concurrently.
+    pub fn start<G, F>(num: usize, init: G) -> impl Iterator<Item = impl FnOnce() + Send>
+    where
+        G: FnOnce(Arsc<Executor>) -> F,
+        F: Future<Output = ()> + Send + 'static,
+    {
+        let workers = (0..num).map(|_| Local::new()).collect::<Vec<_>>();
+
+        let stealers = workers
+            .iter()
+            .map(|w| w.stealer())
+            .collect::<Vec<_>>()
+            .into_boxed_slice();
+
+        let executor = Arsc::new(Executor {
+            injector: SegQueue::new(),
+            stealers,
+            is_shutdown: AtomicBool::new(false),
+        });
+
+        let e2 = executor.clone();
+        let schedule = move |task| e2.injector.push(task);
+        let (init, handle) = async_task::spawn(init(executor.clone()), schedule);
+        init.schedule();
+        handle.detach();
+
+        workers.into_iter().map(move |worker| {
+            let e = executor.clone();
+            || Self::startup(worker, e)
+        })
+    }
+
+    pub fn count(&self) -> usize {
+        self.injector.len() + self.stealers.iter().fold(0, |acc, s| acc + s.len())
+    }
+
+    pub fn spawn<F, T>(&self, fut: F) -> Task<T>
+    where
+        F: Future<Output = T> + Send + 'static,
+        T: Send + 'static,
+    {
+        let (task, handle) = async_task::spawn(fut, WithInfo(Context::enqueue));
+        task.schedule();
+        handle
+    }
+
+    pub fn shutdown(&self) {
+        self.is_shutdown.store(true, Release)
+    }
+
+    fn startup(rq: Local<Runnable, WORKER_CAP>, executor: Arsc<Executor>) {
+        let cx = Context {
+            worker: RefCell::new(Worker {
+                run_queue,
+                preempt_slot: None,
+            }),
+            executor,
+        };
+        CX.set(&cx, || cx.run())
+    }
+}
+
+impl Worker {
+    #[inline]
+    fn pop(&mut self) -> Option<Runnable> {
+        self.preempt_slot.take().or_else(|| self.rq.pop())
+    }
+
+    fn push(&mut self, task: Runnable, injector: &SegQueue<Runnable>, yielded: bool) {
+        if !yielded {
+            if let Some(last) = self.preempt_slot.replace(task) {
+                self.rq.push(last, |task| injector.push(task))
+            }
+        } else {
+            self.rq.push(task, |task| injector.push(task))
+        }
+    }
+}
+
+impl Context {
+    fn next_task(&self, tick: u32, worker: &mut Worker) -> Option<Runnable> {
+        if tick % WORKER_TICK_INTERVAL == 0 {
+            self.executor.injector.pop().or_else(|| worker.pop())
+        } else {
+            worker.pop().or_else(|| self.executor.injector.pop())
+        }
+    }
+
+    fn steal_task(&self, rand: &mut Rng, worker: &mut Worker) -> Option<Runnable> {
+        let stealers = &self.executor.stealers;
+
+        let len = stealers.len();
+        let offset = (rand.next_u64() % len as u64) as usize;
+
+        let mut iter = stealers.iter().cycle().skip(offset).take(len);
+        let task = iter.find_map(|stealer| stealer.steal_into_and_pop(&mut worker.rq));
+        task.or_else(|| self.executor.injector.pop())
+    }
+
+    fn run(&self) {
+        let mut tick = 0u32;
+        let mut rng = rand_riscv::rng();
+        loop {
+            tick = tick.wrapping_add(1);
+
+            if self.executor.shutdown.load(Acquire) {
+                break;
+            }
+
+            let next = self.next_task(tick, &mut self.worker.borrow_mut());
+            if let Some(task) = next {
+                task.run();
+                continue;
+            }
+
+            let stealed = self.steal_task(&mut rng, &mut self.worker.borrow_mut());
+            if let Some(task) = stealed {
+                task.run();
+                continue;
+            }
+
+            core::hint::spin_loop();
+
+            // #[cfg(not(any(target_arch = "riscv32", target_arch =
+            // "riscv64")))] core::hint::spin_loop();
+            // #[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
+            // unsafe {
+            //     core::arch::asm!("wfi")
+            // }
+        }
+    }
+
+    fn enqueue(task: Runnable, sched_info: ScheduleInfo) {
+        let ret = CX.try_with(|cx| {
+            if let Ok(mut worker) = cx.worker.try_borrow_mut() {
+                worker.push(task, &cx.executor.injector, sched_info.woken_while_running);
+            } else {
+                cx.executor.injector.push(task)
+            }
+        });
+        if ret.is_none() {
+            log::warn!("executor exited while scheduling");
+        }
+    }
+}
